7bca0c76f98ee35b239046fe4b22262ab8dc9af3
perceptron8 <perceptron8@users.noreply.github.com>
2022-08-23 20:51:18 +0200

    .

--- js/embed.js
+++ js/embed.js
@@ -3,7 +3,7 @@ import init, { gl_solve } from '../pkg/slowotok.js';
 await init();
 
 document.body.addEventListener("keypress", (event) => {
-	if (event.code === "NumpadEnter") {
+	if (event.key === "Enter") {
 		for (const word of gl_solve({ gl_boardLetters, gl_words}).gl_words) {
 			gl_letters = word;
 			checkWord(word);
