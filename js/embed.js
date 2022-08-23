7ad0e03625637b6478bed05088a222fbce74bae9
perceptron8 <perceptron8@users.noreply.github.com>
2022-08-23 18:21:00 +0200

    .

--- js/embed.js
+++ js/embed.js
@@ -2,9 +2,8 @@ import init, { gl_solve } from '../pkg/slowotok.js';
 
 await init();
 
-document.body.addEventListener("keydown", (event) => {
-	if (event.key === "F7") {
-		event.preventDefault();
+document.body.addEventListener("keypress", (event) => {
+	if (event.code === "NumpadEnter") {
 		for (const word of gl_solve({ gl_boardLetters, gl_words}).gl_words) {
 			gl_letters = word;
 			checkWord(word);
