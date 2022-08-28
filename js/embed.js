5e88ce7e8f39ddb6e1d225c84e9fa7eb702f28d0
perceptron8 <perceptron8@users.noreply.github.com>
2022-08-28 15:51:39 +0200

    auto

--- js/embed.js
+++ js/embed.js
@@ -2,11 +2,32 @@ import init, { gl_solve } from '../pkg/slowotok.js';
 
 await init();
 
+function findWords() {
+	for (const word of gl_solve({gl_boardLetters, gl_words}).gl_words) {
+		gl_letters = word;
+		checkWord(word);
+	}
+}
+
+const connectBoardOriginal = connectBoard;
+window.connectBoard = function() {
+	connectBoardOriginal();
+	findWords();
+}
+
+const sendAnswersOriginal = sendAnswers;
+globalThis.sendAnswers = function() {
+	if (gl_points >= localStorage.gl_points) {
+		sendAnswersOriginal();
+	} else {
+		syncNewGameTime();
+	}
+}
+
 document.body.addEventListener("keypress", (event) => {
 	if (event.key === "Enter") {
-		for (const word of gl_solve({ gl_boardLetters, gl_words}).gl_words) {
-			gl_letters = word;
-			checkWord(word);
-		}
+		findWords();
 	}
 });
+
+findWords();
